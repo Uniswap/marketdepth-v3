3109d7ab0fdb9bccba81ba46c31820fabac8ae74
Austin Adams <36116882+aadams@users.noreply.github.com>
2023-02-28 12:20:48 -0500

    added exact depth calculations

--- src/Depth.sol
+++ src/Depth.sol
@@ -40,8 +40,14 @@ contract Depth {
             });
     }
 
-    function calculateMultipleDepth(address poolAddress, uint256[] calldata sqrtDepthX96, 
-                                    bool[] calldata token0, bool[] calldata both) public returns (uint256[] memory) {
+    function calculateDepth(address poolAddress, uint256 sqrtDepthX96, bool token0, bool bot, bool exact) public returns (uint256) {
+        initializePoolVariables(poolAddress);
+
+        return _calculateDepth(sqrtDepthX96, token0, both, exact);
+    }
+
+    function _calculateMultipleDepth(address poolAddress, uint256[] calldata sqrtDepthX96, 
+                                    bool[] calldata token0, bool[] calldata both, bool exact) public returns (uint256[] memory) {
         require((sqrtDepthX96.length == token0.length) &&
                  sqrtDepthX96.length == both.length, 'Different lengths provided');
 
@@ -52,7 +58,7 @@ contract Depth {
         uint256 tokenAmt;
 
         for (uint i=0; i<sqrtDepthX96.length; i++) {
-            tokenAmt = _calculateDepth(sqrtDepthX96[i], token0[i], both[i]);
+            tokenAmt = _calculateDepth(sqrtDepthX96[i], token0[i], both[i], exact);
 
             returnArray[i] = tokenAmt;
         }
@@ -60,25 +66,19 @@ contract Depth {
         return returnArray;
     }
 
-    function calculateDepth(address poolAddress, uint256 sqrtDepthX96, bool token0, bool both) public returns (uint256) {
-        initializePoolVariables(poolAddress);
-
-        return _calculateDepth(sqrtDepthX96, token0, both);
-    }
-
     // consider initializing the pool variables in a separate function: initializePoolVariables(address pool)
     // that way you could call calculateDepth multiple times in the same txn if you want different depths on the same pool
-    function _calculateDepth(uint256 sqrtDepthX96, bool token0, bool both) private returns (uint256) {
+    function _calculateDepth(uint256 sqrtDepthX96, bool token0, bool both, bool exact) private returns (uint256) {
         uint256 returnAmt = 0;
 
         // put it into global
         token0Ind = token0; 
 
         if (both) {
-            returnAmt+=calculateOneSide(sqrtDepthX96, token0Ind);
-            returnAmt+=calculateOneSide(sqrtDepthX96, !token0Ind);
+            returnAmt+=calculateOneSide(sqrtDepthX96, token0Ind, exact);
+            returnAmt+=calculateOneSide(sqrtDepthX96, !token0Ind, exact);
         } else {
-            returnAmt+=calculateOneSide(sqrtDepthX96, token0Ind);
+            returnAmt+=calculateOneSide(sqrtDepthX96, token0Ind, exact);
         }
        
         return returnAmt;
@@ -86,7 +86,7 @@ contract Depth {
 
     // can just send one bool into this function to determine direction
     // calculateOneSide(bool zeroForOne, uint256 sqrtDepthX96)
-    function calculateOneSide(uint256 sqrtDepthX96, bool upper) private returns (uint256) {
+    function calculateOneSide(uint256 sqrtDepthX96, bool upper, bool exact) private returns (uint256) {
         uint160 sqrtPriceRatioNext = sqrtPriceX96;
         uint160 sqrtPriceX96Current = sqrtPriceX96;
 
@@ -97,6 +97,16 @@ contract Depth {
                                         : uint128(FullMath.mulDiv(sqrtPriceX96, 1 << 96, sqrtDepthX96));
         if (upper) { 
             require(sqrtPriceX96 <= sqrtPriceX96Tgt);
+        } else  if (exact) {
+            // we want to calculate deflator = (1-p)^2 / 2 to approximate (1-p) instead of 1/(1+p)
+            // because 1 / (1 + p) * price * (1-deflator) = (1-p) * price
+            // this is because of the taylor series expansion of these values
+            // however we need to keep everything in integers, so we cannot directly calculate sqrt(1-p)
+            // 112045541949572287496682733568 = sqrt(2) * 2^96, which breaks this code/deflation
+            require(sqrtDepthX96 < 112045541949572287496682733568);
+
+            uint256 deflator = (sqrtDepthX96 * sqrtDepthX96 - (4 * (1 << 96)) - (1 << 192)) / (1 << 96);
+            sqrtPriceX96Tgt = uint128(FullMath.mulDiv(sqrtPriceX96Tgt, ((1 << 192) - (deflator * deflator) / 2),  1 << 192));
         }
 
         // todo ask q
