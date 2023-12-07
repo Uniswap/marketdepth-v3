b19b572c2aa79f8a0870326c71ed290cfc3b1c17
Austin Adams <austino256@gmail.com>
2023-12-07 11:31:10 -0500

    change license carryover from v3-core

--- src/PoolTickBitmap.sol
+++ src/PoolTickBitmap.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.6;
 
 import {IUniswapV3Pool} from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
